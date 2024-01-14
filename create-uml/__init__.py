eba7f43eb41f7a0015fc11dc63b875ea5b53b78d
song zhao <songzhao@songdeMacBook-Pro.local>
2024-01-14 19:36:38 +0800

    [Infra] remove create uml

--- create-uml/__init__.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python
-# Copyright by OSCode in 2021, All Rights Reserved.
--- envsetup.sh
+++ envsetup.sh
@@ -13,10 +13,7 @@ export PYTHONPATH=${ROOT_PATH}:$PYTHONPATH
 # path of code-format
 PATH_CODE_CHECK=${ROOT_PATH}"/code-format"
 export PATH=${PATH}:${PATH_CODE_CHECK}
-# path of create-uml
-PATH_CREATE_UML=${ROOT_PATH}"/create-uml"
-export PATH=${PATH}:${PATH_CREATE_UML}
 
 
 alias code-check="code-check.py"
-alias create-uml="create-uml.py"
+
